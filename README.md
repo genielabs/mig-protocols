875dbd3a2b08f17b20bf3d44442ac95c05f86f72
Generoso Martello <generoso.martello@gmail.com>
2018-09-24 19:28:20 +0200

    Initial commit: detached from https://github.com/genielabs/mig-service-dotnet

---
+++ README.md
@@ -0,0 +1,26 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/m9gpdbs7toa1348y?svg=true)](https://ci.appveyor.com/project/genemars/mig-homeauto)
+[![NuGet](https://img.shields.io/nuget/v/MIG.HomeAutomation.svg)](https://www.nuget.org/packages/MIG.HomeAutomation/)
+![License](https://img.shields.io/github/license/genielabs/mig-homeauto.svg)
+
+# API for Home Automation (X10 and Z-Wave)
+
+Adds Home Automation API commands to *[MIG](https://github.com/genielabs/mig-service-dotnet)*:
+
+- [HomeAutomation.X10 API](https://genielabs.github.io/HomeGenie/api/mig/mig_api_x10.html)
+- [HomeAutomation.ZWave API](https://genielabs.github.io/HomeGenie/api/mig/mig_api_zwave.html)
+
+## Supported Hardware
+
+- **X10**: CM11 and CM15+RF
+- **Z-Wave**: most z-wave serial controllers
+
+## Related Projects
+
+- https://github.com/genielabs/mig-service-dotnet
+- https://github.com/genielabs/x10-lib-dotnet
+- https://github.com/genielabs/zwave-lib-dotnet
+- https://github.com/genielabs/homegenie
+
+## License
+
+This project is open source software, licensed under the terms of Apache license 2.0. See the [LICENSE](LICENSE) file for details.
---
+++ Test.WebService/Program.cs
@@ -0,0 +1,69 @@
+﻿/*
+  This file is part of MIG (https://github.com/genielabs/mig-service-dotnet)
+ 
+  Copyright (2012-2018) G-Labs (https://github.com/genielabs)
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+using System;
+using System.IO;
+using System.Threading;
+using System.Xml.Serialization;
+
+using MIG;
+using MIG.Config;
+
+namespace Test.WebService
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string webPort = "8088";
+
+            Console.WriteLine("MigService test APP");
+            Console.WriteLine("URL: http://localhost:{0}", webPort);
+
+            var migService = new MigService();
+
+            // Add and configure the Web gateway
+            var web = migService.AddGateway("WebServiceGateway");
+            web.SetOption("HomePath", "html");
+            web.SetOption("BaseUrl", "/pages/");
+            web.SetOption("Host", "*");
+            web.SetOption("Port", webPort);
+            web.SetOption("Password", "");
+            web.SetOption("EnableFileCaching", "False");
+
+            /*
+            // Add and configure the Web Socket gateway
+            var ws = migService.AddGateway("WebSocketGateway");
+            ws.SetOption("Port", "8181");
+            */
+            
+            migService.StartService();
+
+            // Enable UPnP interface
+
+            var upnp = migService.AddInterface("Protocols.UPnP", "MIG.Protocols.dll");
+            migService.EnableInterface("Protocols.UPnP");
+
+            while (true)
+            {
+                Thread.Sleep(10000);
+            }
+
+        }
+    }
+}
